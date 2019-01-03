e655f5aee6fddf38f7a4f031926c3afa165f6970
Remco Weekers <remco@craftsmen.nl>
2019-01-03 18:53:23 +0100

    Initial commit

---
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/VoornameninliedjesBackendApplication.kt
@@ -0,0 +1,12 @@
+package nl.orangeflamingo.voornameninliedjesbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VoornameninliedjesBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<VoornameninliedjesBackendApplication>(*args)
+}
+
