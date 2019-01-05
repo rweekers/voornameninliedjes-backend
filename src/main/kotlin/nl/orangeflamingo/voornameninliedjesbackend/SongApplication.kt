d48f0bae305e5131b6d5d34a99ea99342364dd0c
Remco Weekers <remco@craftsmen.nl>
2019-01-05 12:19:55 +0100

    Add some dummy data

---
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/SongApplication.kt
@@ -0,0 +1,44 @@
+package nl.orangeflamingo.voornameninliedjesbackend
+
+import nl.orangeflamingo.voornameninliedjesbackend.domain.Song
+import nl.orangeflamingo.voornameninliedjesbackend.repository.SongRepository
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import reactor.core.publisher.Flux
+
+@SpringBootApplication
+open class SongApplication {
+
+	private val log = LoggerFactory.getLogger(SongApplication::class.java)
+
+	@Bean
+	open fun init(repository: SongRepository) = CommandLineRunner {
+//			val mongo = MongoClient()//("localhost", 27017);
+//			val template = MongoTemplate(mongo, "local")
+//			val songCollectionName = template.getCollectionName(Song::class.java)
+//
+//			if (template.collectionExists(songCollectionName)) {
+//				template.dropCollection(songCollectionName)
+//			}
+//
+//			// Capped collections need to be created manually
+//			template.createCollection(songCollectionName, CollectionOptions.empty().capped().size(9999999L))//.maxDocuments(100L));
+
+			val songFlowable = Flux.just(
+					Song("1", "Michael Jackson", "Ben", "Ben"),
+					Song("2", "Neil Diamond", "Sweet Caroline", "Caroline"),
+					Song("3", "The Police", "Roxanne", "Roxanne"),
+					Song("4", "Dolly Parton", "Jolene", "Jolene"),
+					Song("5", "The Kinks", "Lola", "Lola")
+			)
+
+			repository.saveAll(songFlowable).thenMany<Song>{ repository.findAll() }.subscribe{ song -> log.info(song.toString()) }
+	}
+}
+
+fun main(args: Array<String>) {
+	runApplication<SongApplication>(*args)
+}
--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/controller/SongController.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/controller/SongController.kt
@@ -1,18 +1,27 @@
 package nl.orangeflamingo.voornameninliedjesbackend.controller
 
 import nl.orangeflamingo.voornameninliedjesbackend.domain.Song
+import nl.orangeflamingo.voornameninliedjesbackend.repository.SongRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @RestController
 class SongController {
 
-    @GetMapping("/songs/")
-    fun getSongs() =
-            listOf(Song("test", "name", "Hello, name", "test2"))
+    @Autowired
+    private lateinit var songRepository: SongRepository
+
+    @GetMapping("/songs")
+    fun getSongs(): Flux<Song> {
+        return songRepository.findAll()
+    }
 
     @GetMapping("/songs/{id}")
-    fun getSongById(@PathVariable("id") id: String) =
-            Song(id, "bla", "die bla", "bla2")
+    fun getSongById(@PathVariable("id") id: String): Mono<Song> {
+        return songRepository.findById(id)
+    }
 }
