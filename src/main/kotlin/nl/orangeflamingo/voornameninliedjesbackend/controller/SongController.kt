e36f3d225b5e915e13f326254a23b9062bb32694
Remco Weekers <remco@craftsmen.nl>
2019-01-12 12:56:51 +0100

    Add api to standard path

--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/controller/SongController.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/controller/SongController.kt
@@ -5,11 +5,13 @@ import nl.orangeflamingo.voornameninliedjesbackend.repository.SongRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
+@RequestMapping("/api")
 class SongController {
 
     @Autowired
