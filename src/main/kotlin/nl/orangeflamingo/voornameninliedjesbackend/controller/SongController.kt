6a2e4b94b6c2cff3ba0df8ebb0a685c8a61d9128
Remco Weekers <remco@craftsmen.nl>
2019-02-26 08:36:47 +0100

    Add CORS setting, WiP, need proper setup

--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/controller/SongController.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/controller/SongController.kt
@@ -5,10 +5,7 @@ import nl.orangeflamingo.voornameninliedjesbackend.domain.SongStatus
 import nl.orangeflamingo.voornameninliedjesbackend.dto.SongDto
 import nl.orangeflamingo.voornameninliedjesbackend.repository.SongRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
@@ -20,7 +17,7 @@ class SongController {
     private lateinit var songRepository: SongRepository
 
     @GetMapping("/songs")
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = ["http://localhost:3000"])
     fun getSongs(): Flux<SongDto> {
         return songRepository.findAllByStatus(SongStatus.SHOW).map { convertToDto(it) }
     }
