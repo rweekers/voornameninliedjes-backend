5bf11876a4ffda4996d8e65bc57e92def3218127
Remco Weekers <remco.weekers@gmail.com>
2019-11-19 22:55:50 +0100

    Gradle docker plugin setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,32 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+import com.bmuschko.gradle.docker.tasks.container.*
+import com.bmuschko.gradle.docker.tasks.image.*
+
+val buildMyAppImage by tasks.creating(DockerBuildImage::class) {
+    inputDir.set(file("."))
+    images.add("mongo:latest")
+}
+
+val createMyAppContainer by tasks.creating(DockerCreateContainer::class) {
+    dependsOn(buildMyAppImage)
+    targetImageId(buildMyAppImage.getImageId())
+    hostConfig.portBindings.set(listOf("27017:27017"))
+}
+
+val startMyAppContainer by tasks.creating(DockerStartContainer::class) {
+    dependsOn(createMyAppContainer)
+    targetContainerId(createMyAppContainer.getContainerId())
+}
+
+val stopMyAppContainer by tasks.creating(DockerStopContainer::class) {
+    targetContainerId(createMyAppContainer.containerId)
+}
+
 plugins {
     id("org.springframework.boot") version "2.2.1.RELEASE"
     id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    id("com.bmuschko.docker-remote-api") version "6.0.0"
     kotlin("jvm") version "1.3.50"
     kotlin("plugin.spring") version "1.3.50"
 }
@@ -23,16 +47,13 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-    }
     testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+tasks.create("functionalTestMyApp", Test::class) {
+    dependsOn(startMyAppContainer)
+    finalizedBy(stopMyAppContainer)
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/kotlin/nl/orangeflamingo/voornameninliedjesbackend/SongControllerTest.kt
@@ -0,0 +1,12 @@
+package nl.orangeflamingo.voornameninliedjesbackend
+
+import org.junit.Test
+
+class SongControllerTest {
+
+	@Test
+	fun testGetAllSongs() {
+	}
+
+}
+
