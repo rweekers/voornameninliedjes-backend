415154a28a0952b207e5b0d6ec0898ab08ba3b48
Remco Weekers <remco.weekers@gmail.com>
2020-01-31 08:58:03 +0100

    Update to Java 11.

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,6 +90,6 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
