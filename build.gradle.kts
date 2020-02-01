201d4f24024993113505b560986236ca57c9fe75
Remco Weekers <remco.weekers@gmail.com>
2020-02-01 10:12:53 +0100

    Update to Java 11 Kotlin sources compatibility.

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,7 +68,7 @@ sonarqube {
 
 group = "nl.orangeflamingo"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
