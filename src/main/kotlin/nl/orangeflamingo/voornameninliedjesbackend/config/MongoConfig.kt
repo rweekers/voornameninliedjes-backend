63f418af845a5d3cb09dfa8187095af9fcf41d13
Remco Weekers <remco.weekers@gmail.com>
2020-12-17 17:18:11 +0100

    Configure indices for mongodb

---
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/config/MongoConfig.kt
@@ -0,0 +1,26 @@
+package nl.orangeflamingo.voornameninliedjesbackend.config
+
+import nl.orangeflamingo.voornameninliedjesbackend.domain.Song
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.event.EventListener
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.convert.MongoConverter
+import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver
+
+@Configuration
+class MongoConfig {
+
+    @Autowired
+    private lateinit var mongoTemplate: MongoTemplate
+    @Autowired
+    private lateinit var mongoConverter: MongoConverter
+
+    @EventListener(ApplicationReadyEvent::class)
+    fun initIndicesAfterStartup() {
+        val indexOps = mongoTemplate.indexOps(Song::class.java)
+        val resolver = MongoPersistentEntityIndexResolver(mongoConverter.mappingContext)
+        resolver.resolveIndexFor(Song::class.java).forEach { indexOps.ensureIndex(it) }
+    }
+}
