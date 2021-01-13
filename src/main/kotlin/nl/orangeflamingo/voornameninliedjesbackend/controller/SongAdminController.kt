a62c55ad6dbae9213f083ed361148120544d8c45
Remco Weekers <remco.weekers@gmail.com>
2021-01-13 23:43:37 +0100

    Return adminSongDto, extend tests for endpoints.

--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/controller/SongAdminController.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/controller/SongAdminController.kt
@@ -1,8 +1,11 @@
 package nl.orangeflamingo.voornameninliedjesbackend.controller
 
+import nl.orangeflamingo.voornameninliedjesbackend.domain.*
 import nl.orangeflamingo.voornameninliedjesbackend.repository.postgres.SongRepository
+import nl.orangeflamingo.voornameninliedjesbackend.service.SongService
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.annotation.CachePut
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 
@@ -14,6 +17,25 @@ class SongAdminController {
 
     @Autowired
     private lateinit var songRepository: SongRepository
+
+    @Autowired
+    private lateinit var songService: SongService
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/songs")
+    @CachePut(value = ["songs"])
+    @CrossOrigin(origins = ["http://localhost:3000", "https://voornameninliedjes.nl", "*"])
+    fun getSongs(): List<AdminSongDto> {
+        return songService.findAll()
+            .map { convertToDto(it) }
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/songs/{id}")
+    @CrossOrigin(origins = ["http://localhost:3000", "https://voornameninliedjes.nl", "*"])
+    fun getSongById(@PathVariable("id") id: Long): AdminSongDto {
+        return convertToDto(songService.findById(id))
+    }
 
     @PreAuthorize("hasRole('ROLE_OWNER')")
     @DeleteMapping("/songs")
@@ -30,5 +52,44 @@ class SongAdminController {
     fun deleteSongById(@PathVariable id: Long) {
         songRepository.deleteById(id)
         log.info("song $id deleted")
+    }
+
+    private fun convertToDto(song: AggregateSong): AdminSongDto {
+        return AdminSongDto(
+            id = song.id.toString(),
+            artist = song.artistName,
+            title = song.title,
+            name = song.name,
+            artistImage = song.artistImage,
+            background = song.background,
+            youtube = song.youtube,
+            spotify = song.spotify,
+            status = song.status.name,
+            wikimediaPhotos = song.wikimediaPhotos.map { w -> convertToDto(w) }.toSet(),
+            flickrPhotos = song.flickrPhotos.map { it.flickrId }.toSet(),
+            sources = song.sources.map { s -> convertToDto(s) }.toSet(),
+            logs = song.logEntries.map { convertToDto(it) }
+        )
+    }
+
+    private fun convertToDto(it: SongLogEntry): AdminLogEntry {
+        return AdminLogEntry(
+            date = it.date,
+            user = it.username
+        )
+    }
+
+    private fun convertToDto(wikimediaPhoto: ArtistWikimediaPhoto): AdminWikimediaPhotoDto {
+        return AdminWikimediaPhotoDto(
+            url = wikimediaPhoto.url,
+            attribution = wikimediaPhoto.attribution
+        )
+    }
+
+    private fun convertToDto(songSource: SongSource): AdminSourceDto {
+        return AdminSourceDto(
+            url = songSource.url,
+            name = songSource.name
+        )
     }
 }
--- src/test/kotlin/nl/orangeflamingo/voornameninliedjesbackend/SongControllerTest.kt
+++ src/test/kotlin/nl/orangeflamingo/voornameninliedjesbackend/SongControllerTest.kt
@@ -3,6 +3,8 @@ package nl.orangeflamingo.voornameninliedjesbackend
 import nl.orangeflamingo.voornameninliedjesbackend.domain.DbSong
 import nl.orangeflamingo.voornameninliedjesbackend.domain.User
 import nl.orangeflamingo.voornameninliedjesbackend.domain.UserRole
+import nl.orangeflamingo.voornameninliedjesbackend.repository.mongo.MongoSongRepository
+import nl.orangeflamingo.voornameninliedjesbackend.repository.postgres.SongRepository
 import nl.orangeflamingo.voornameninliedjesbackend.repository.postgres.UserRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -20,20 +22,25 @@ import kotlin.text.Charsets.UTF_8
 class SongControllerTest(
     @Autowired val client: WebTestClient,
     @Autowired val userRepository: UserRepository,
+    @Autowired val mongoSongRepository: MongoSongRepository,
+    @Autowired val songRepository: SongRepository,
     @Autowired val encoder: PasswordEncoder
 ) {
 
     private val user: String = "test"
     private val password: String = "secret"
-    private val role: String = "ADMIN"
+    private val adminRole: String = "ADMIN"
 
     @BeforeEach
     fun createUser() {
+        mongoSongRepository.deleteAll()
+        songRepository.deleteAll()
+        userRepository.deleteAll()
         userRepository.save(
             User(
                 username = user,
                 password = encoder.encode(password),
-                roles = mutableSetOf(UserRole(role))
+                roles = mutableSetOf(UserRole(adminRole))
             )
         )
     }
@@ -42,12 +49,38 @@ class SongControllerTest(
     fun songControllerTest() {
         client.get()
             .uri("/api/songs")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList<DbSong>().hasSize(0)
+    }
+
+    @Test
+    fun songControllerBetaTest() {
+        client.get()
+            .uri("/beta/songs")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList<DbSong>().hasSize(0)
+    }
+
+    @Test
+    fun songAdminControllerBetaTest() {
+        client.get()
+            .uri("/gamma/songs")
             .header(
                 "Authorization", "Basic ${Base64Utils.encodeToString("$user:$password".toByteArray(UTF_8))}"
             )
             .exchange()
             .expectStatus().isOk
             .expectBodyList<DbSong>().hasSize(0)
+    }
+
+    @Test
+    fun songAdminControllerBetaUnauthorizedTest() {
+        client.get()
+            .uri("/gamma/songs")
+            .exchange()
+            .expectStatus().isUnauthorized
     }
 
 }
