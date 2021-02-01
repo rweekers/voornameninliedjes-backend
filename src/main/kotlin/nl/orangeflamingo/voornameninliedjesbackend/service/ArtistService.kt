7c532ed23dd2f529e0e1bd49aba0c7ddb96034c9
Remco Weekers <remco.weekers@gmail.com>
2021-02-01 21:18:26 +0100

    Small cleanup

--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/service/ArtistService.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/service/ArtistService.kt
@@ -1,24 +1,15 @@
 package nl.orangeflamingo.voornameninliedjesbackend.service
 
-import nl.orangeflamingo.voornameninliedjesbackend.domain.AggregateSong
 import nl.orangeflamingo.voornameninliedjesbackend.domain.Artist
 import nl.orangeflamingo.voornameninliedjesbackend.repository.postgres.ArtistRepository
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class ArtistService {
 
-    private val log = LoggerFactory.getLogger(ArtistService::class.java)
-
     @Autowired
     private lateinit var artistRepository: ArtistRepository
-
-    fun countArtists(): Long {
-        log.info("Count artists")
-        return artistRepository.count()
-    }
 
     fun findByName(name: String): List<Artist> {
         return artistRepository.findByNameIgnoreCase(name)
