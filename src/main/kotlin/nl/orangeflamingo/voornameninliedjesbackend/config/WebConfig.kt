01048114317c9a9650636d5901a3db6fb87795a0
Remco Weekers <remco@weekers.dev>
2022-01-29 09:24:42 +0100

    Adjust path to be configurable with docker volume

--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/config/WebConfig.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/config/WebConfig.kt
@@ -1,5 +1,6 @@
 package nl.orangeflamingo.voornameninliedjesbackend.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.CacheControl
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
@@ -11,9 +12,12 @@ import java.time.Duration
 @EnableWebMvc
 class WebConfig : WebMvcConfigurer {
 
+    @Value("\${voornameninliedjes.images.path}")
+    private val imagesPath: String = "images"
+
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/images/**")
-            .addResourceLocations("file:images/")
+            .addResourceLocations("file:${imagesPath}/")
             .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)))
     }
 }
