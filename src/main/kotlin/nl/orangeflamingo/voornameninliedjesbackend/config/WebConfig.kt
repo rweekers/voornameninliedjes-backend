55fc7d48a2e717cd76ef977b7efbc86e5f9dda6b
Remco Weekers <remco@weekers.dev>
2023-06-11 10:40:19 +0200

    Remove exposing images as resource

--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/config/WebConfig.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/config/WebConfig.kt
@@ -1,22 +1,15 @@
 package nl.orangeflamingo.voornameninliedjesbackend.config
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.CacheControl
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import java.time.Duration
 
 @Configuration
 @EnableWebMvc
 class WebConfig(
     private val corsConfig: CorsConfig
 ) : WebMvcConfigurer {
-
-    @Value("\${voornameninliedjes.images.path}")
-    private val imagesPath: String = "images"
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/**")
@@ -28,10 +21,5 @@ class WebConfig(
             .allowedOrigins(*corsConfig.admin.toTypedArray())
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowCredentials(true).maxAge(3600)
-    }
-    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry.addResourceHandler("/images/**")
-            .addResourceLocations("file:${imagesPath}/")
-            .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)))
     }
 }
