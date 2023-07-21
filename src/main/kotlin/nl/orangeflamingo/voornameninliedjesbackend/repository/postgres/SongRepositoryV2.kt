4d5934dfc0c88020f1dbd10dd7111ccb4153e379
Remco Weekers <remco@weekers.dev>
2023-07-21 17:02:08 +0200

    Return songdto.

--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/repository/postgres/SongRepositoryV2.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/repository/postgres/SongRepositoryV2.kt
@@ -2,8 +2,8 @@ package nl.orangeflamingo.voornameninliedjesbackend.repository.postgres
 
 import nl.orangeflamingo.voornameninliedjesbackend.domain.Song
 import nl.orangeflamingo.voornameninliedjesbackend.domain.SongStatus
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
@@ -11,5 +11,5 @@ import org.springframework.stereotype.Repository
 @Repository
 interface SongRepositoryV2: PagingAndSortingRepository<Song, Long> {
 
-    fun findByNameStartingWithAndStatusOrderByName(lastname: String?, status: SongStatus, pageable: Pageable): Page<Song>
+    fun findByNameStartingWithIgnoreCaseAndStatusOrderByName(firstChars: String, status: SongStatus, pageable: Pageable): Slice<Song>
 }
