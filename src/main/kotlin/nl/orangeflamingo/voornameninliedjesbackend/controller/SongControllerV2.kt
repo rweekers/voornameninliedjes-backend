63646aa99f364fd1aa503755e03c833f0747e148
Remco Weekers <remco@weekers.dev>
2023-07-21 17:02:08 +0200

    Return songdto.

--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/controller/SongControllerV2.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/controller/SongControllerV2.kt
@@ -1,21 +1,13 @@
 package nl.orangeflamingo.voornameninliedjesbackend.controller
 
-import nl.orangeflamingo.voornameninliedjesbackend.domain.AggregateSong
-import nl.orangeflamingo.voornameninliedjesbackend.domain.ArtistWikimediaPhoto
-import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmTagDto
-import nl.orangeflamingo.voornameninliedjesbackend.domain.PhotoDetail
+import nl.orangeflamingo.voornameninliedjesbackend.domain.Artist
 import nl.orangeflamingo.voornameninliedjesbackend.domain.Song
 import nl.orangeflamingo.voornameninliedjesbackend.domain.SongStatus
-import nl.orangeflamingo.voornameninliedjesbackend.domain.SongWikimediaPhoto
-import nl.orangeflamingo.voornameninliedjesbackend.dto.FlickrLicenseDto
-import nl.orangeflamingo.voornameninliedjesbackend.dto.FlickrOwnerDto
-import nl.orangeflamingo.voornameninliedjesbackend.dto.PhotoDto
 import nl.orangeflamingo.voornameninliedjesbackend.dto.SongDto
 import nl.orangeflamingo.voornameninliedjesbackend.dto.SourceDto
-import nl.orangeflamingo.voornameninliedjesbackend.dto.WikimediaPhotoDto
-import nl.orangeflamingo.voornameninliedjesbackend.repository.postgres.SongRepositoryV2
-import org.springframework.data.domain.Page
+import nl.orangeflamingo.voornameninliedjesbackend.service.SongServiceV2
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -24,22 +16,21 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api")
 class SongControllerV2(
-    private val songRepositoryV2: SongRepositoryV2
+    private val songServiceV2: SongServiceV2
 ) {
 
-    @GetMapping("/songs", params = ["first-character", "page-size", "page-number"])
+    @GetMapping("/songs", params = ["name-starts-with", "page-number"])
     fun getSongsWithNameStartingWith(
-        @RequestParam(name = "first-character") firstCharacter: String,
-        @RequestParam(name = "page-size") pageSize: String,
+        @RequestParam(name = "name-starts-with") nameStartsWith: String,
         @RequestParam(name = "page-number") pageNumber: String
-    ): Page<Song> {
-        return songRepositoryV2.findByNameStartingWithAndStatusOrderByName(firstCharacter, SongStatus.SHOW, Pageable.ofSize(pageSize.toInt()).withPage(pageNumber.toInt()))
-            //.map { convertToDto(it, emptyList()) }
+    ): Slice<SongDto> {
+        return songServiceV2.findByNameStartingWith(nameStartsWith, SongStatus.SHOW, Pageable.ofSize(30).withPage(pageNumber.toInt()))
+            .map { convertToDto(it.first, it.second) }
     }
 
-    private fun convertToDto(song: AggregateSong, photos: List<PhotoDetail>): SongDto {
+    private fun convertToDto(song: Song, artist: Artist): SongDto {
         return SongDto(
-            artist = song.artistName,
+            artist = artist.name,
             title = song.title,
             name = song.name,
             artistImage = song.artistImage,
@@ -48,7 +39,7 @@ class SongControllerV2(
             blurredImage = song.blurredImage,
             artistImageWidth = song.artistImageWidth,
             artistImageHeight = song.artistImageHeight,
-            artistLastFmUrl = song.artistLastFmUrl,
+            artistLastFmUrl = artist.lastFmUrl,
             wikipediaPage = song.wikipediaPage,
             youtube = song.youtube,
             lastFmUrl = song.lastFmUrl,
@@ -56,60 +47,19 @@ class SongControllerV2(
             albumLastFmUrl = song.albumLastFmUrl,
             spotify = song.spotify,
             background = song.background,
-            wikipediaNl = song.wikipediaContentNl,
-            wikipediaEn = song.wikipediaContentEn,
-            wikipediaSummaryEn = song.wikipediaSummaryEn,
-            wikimediaPhotos = mergeAndConvertWikimediaPhotos(song.artistWikimediaPhotos, song.songWikimediaPhotos),
-            flickrPhotos = photos.map {
-                PhotoDto(
-                    id = it.id,
-                    url = it.url,
-                    farm = it.farm,
-                    secret = it.secret,
-                    server = it.server,
-                    title = it.title,
-                    owner = FlickrOwnerDto(
-                        id = it.ownerDetail?.id ?: "",
-                        username = it.ownerDetail?.username ?: "",
-                        photoUrl = it.ownerDetail?.photosUrl ?: ""
-                    ),
-                    license = FlickrLicenseDto(
-                        name = it.licenseDetail?.name ?: "",
-                        url = it.licenseDetail?.url ?: ""
-                    )
-                )
-            }.toSet(),
+            wikipediaNl = null,
+            wikipediaEn = null,
+            wikipediaSummaryEn = null,
+            wikimediaPhotos = emptySet(),
+            flickrPhotos = emptySet(),
             sources = song.sources.map {
                 SourceDto(
                     url = it.url,
                     name = it.name
                 )
             }.toSet(),
-            tags = song.tags.map {
-                LastFmTagDto(
-                    name = it.name,
-                    url = it.url
-                )
-            }.toSet(),
+            tags = emptySet(),
             hasDetails = song.hasDetails
         )
-    }
-
-    private fun mergeAndConvertWikimediaPhotos(
-        artistWikimediaPhoto: Set<ArtistWikimediaPhoto>,
-        songWikimediaPhotos: Set<SongWikimediaPhoto>
-    ): Set<WikimediaPhotoDto> {
-        val mergedPhotos = mutableSetOf<WikimediaPhotoDto>()
-        mergedPhotos.addAll(songWikimediaPhotos.map { convertSongWikimediaPhotoToDto(it) })
-        mergedPhotos.addAll(artistWikimediaPhoto.map { convertArtistWikimediaPhotoToDto(it) })
-        return mergedPhotos
-    }
-
-    private fun convertArtistWikimediaPhotoToDto(wikimediaPhoto: ArtistWikimediaPhoto): WikimediaPhotoDto {
-        return WikimediaPhotoDto(wikimediaPhoto.url, wikimediaPhoto.attribution)
-    }
-
-    private fun convertSongWikimediaPhotoToDto(wikimediaPhoto: SongWikimediaPhoto): WikimediaPhotoDto {
-        return WikimediaPhotoDto(wikimediaPhoto.url, wikimediaPhoto.attribution)
     }
 }
