76123a5017ef1b3f066c75eb1636d2ed167050bc
Remco Weekers <remco@weekers.dev>
2023-11-25 11:38:31 +0100

    Fix code smells

--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/client/LastFmApiClient.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/client/LastFmApiClient.kt
@@ -3,7 +3,7 @@ package nl.orangeflamingo.voornameninliedjesbackend.client
 import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmResponse
 import reactor.core.publisher.Mono
 
-interface LastFmApiClient {
+fun interface LastFmApiClient {
 
     fun getTrack(artist: String, title: String): Mono<LastFmResponse>
 }
--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/client/WikipediaApiClient.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/client/WikipediaApiClient.kt
@@ -3,7 +3,7 @@ package nl.orangeflamingo.voornameninliedjesbackend.client
 import nl.orangeflamingo.voornameninliedjesbackend.domain.WikipediaApi
 import reactor.core.publisher.Mono
 
-interface WikipediaApiClient {
+fun interface WikipediaApiClient {
 
     fun getBackground(wikipediaPage: String): Mono<WikipediaApi>
 }
