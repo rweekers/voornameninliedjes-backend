78ea9fbcf6e8a72d61607e49fe5019a59a908738
Remco Weekers <remco@weekers.dev>
2023-12-23 09:43:26 +0100

    Cleanup unused columns and table

---
+++ src/main/resources/db/migration/postgresql/prod/V27__Remove_key_columns.sql
@@ -0,0 +1,4 @@
+ALTER TABLE artist_log_entries DROP COLUMN artist_key;
+ALTER TABLE song_last_fm_tags DROP COLUMN song_key;
+ALTER TABLE song_log_entries DROP COLUMN song_key;
+ALTER TABLE song_sources DROP COLUMN song_key;
