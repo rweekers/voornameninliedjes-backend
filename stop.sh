7b855bb32848ee5a488cf22320dd7855f505f8c4
Remco Weekers <remco@weekers.dev>
2023-12-28 12:15:47 +0100

    Kibana setup (WiP)

--- stop.sh
+++ stop.sh
@@ -4,6 +4,12 @@ echo 'stopping containers...'
 
 docker stop some-postgres
 docker rm some-postgres
+docker stop elasticsearch
+docker rm elasticsearch
+docker stop kibana
+docker rm kibana
+docker stop logstash
+docker rm logstash
 
 echo 'containers stopped...'
 
