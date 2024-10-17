3127fe6d47680dafd32a8df8da1ee8e1dada8ac6
Remco Weekers <remco@weekers.dev>
2024-10-17 16:46:00 +0200

    chore: use immutable properties for statistics

--- src/main/java/nl/orangeflamingo/voornameninliedjesbackend/domain/Statistics.kt
+++ src/main/java/nl/orangeflamingo/voornameninliedjesbackend/domain/Statistics.kt
@@ -1,13 +1,13 @@
 package nl.orangeflamingo.voornameninliedjesbackend.domain
 
 data class SongNameStatistics(
-    var name: String,
-    var count: Int,
+    val name: String,
+    val count: Int,
 )
 
 data class ArtistNameStatistics(
-    var name: String,
-    var count: Int,
+    val name: String,
+    val count: Int,
 )
 
 data class SongStatistics(
