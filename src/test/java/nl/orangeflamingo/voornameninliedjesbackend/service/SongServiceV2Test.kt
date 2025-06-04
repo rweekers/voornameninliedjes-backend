e39e815620ba3268d126e042209e060edf2a0a21
Remco Weekers <remco@weekers.dev>
2025-06-04 14:00:29 +0200

    test: extend test for SongServiceV2

--- src/test/java/nl/orangeflamingo/voornameninliedjesbackend/service/SongServiceV2Test.kt
+++ src/test/java/nl/orangeflamingo/voornameninliedjesbackend/service/SongServiceV2Test.kt
@@ -1,16 +1,23 @@
 package nl.orangeflamingo.voornameninliedjesbackend.service
 
+import nl.orangeflamingo.voornameninliedjesbackend.domain.Artist
+import nl.orangeflamingo.voornameninliedjesbackend.domain.Photo
+import nl.orangeflamingo.voornameninliedjesbackend.domain.Song
+import nl.orangeflamingo.voornameninliedjesbackend.domain.SongDetail
+import nl.orangeflamingo.voornameninliedjesbackend.domain.SongPhoto
 import nl.orangeflamingo.voornameninliedjesbackend.domain.SongStatus
 import nl.orangeflamingo.voornameninliedjesbackend.domain.SongWithArtist
 import nl.orangeflamingo.voornameninliedjesbackend.repository.postgres.ArtistRepository
 import nl.orangeflamingo.voornameninliedjesbackend.repository.postgres.SongDetailRepository
 import nl.orangeflamingo.voornameninliedjesbackend.repository.postgres.SongRepositoryV2
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jdbc.core.mapping.AggregateReference
+import java.util.Optional
 
 class SongServiceV2Test {
 
@@ -36,5 +43,43 @@ class SongServiceV2Test {
         val songs = songServiceV2.findByNameStartingWith(name, status, Pageable.ofSize(limit).withPage(page))
         assertThat(songs).isEqualTo(listOf(song))
         verify(songRepositoryV2).findAllSongsWithArtistsStartingWith(status = "SHOW", name = "Al", limit = 10, offset = page)
+    }
+
+    @Test
+    fun `find by artist and title`() {
+        val artist = "Paul Simon"
+        val title = "You Can Call Me Al"
+        val name = "Al"
+        val photoUrl = "https://someUrl"
+        val photoAttribution = "Some attribution"
+        val songEntity = mock(Song::class.java)
+        val artistEntity = mock(Artist::class.java)
+        val songPhoto = mock(SongPhoto::class.java)
+        val artistAggregate: AggregateReference<Artist, Long> = mock(AggregateReference::class.java) as AggregateReference<Artist, Long>
+        val artistId = 1L
+        whenever(songEntity.title).thenReturn(title)
+        whenever(songEntity.name).thenReturn(name)
+        whenever(songPhoto.url).thenReturn(photoUrl)
+        whenever(songPhoto.attribution).thenReturn(photoAttribution)
+        whenever(songEntity.photos).thenReturn(mutableSetOf(songPhoto))
+        whenever(artistEntity.name).thenReturn(artist)
+        whenever(songEntity.artist).thenReturn(artistAggregate)
+        whenever(artistAggregate.id).thenReturn(artistId)
+        whenever(songDetailRepository.findByArtistAndTitle(artist, title)).thenReturn(Optional.of(songEntity))
+        whenever(artistRepository.findById(artistAggregate.id)).thenReturn(Optional.of(artistEntity))
+        val song = songServiceV2.findByArtistAndTitle(artist, title)
+
+        val expectedSong =
+            SongDetail(
+                artist = artist,
+                title = title,
+                name = name,
+                artistImageWidth = 0,
+                artistImageHeight = 0,
+                photos = listOf(
+                    Photo(photoUrl, photoAttribution)
+                )
+            )
+        assertThat(song).isEqualTo(expectedSong)
     }
 }
