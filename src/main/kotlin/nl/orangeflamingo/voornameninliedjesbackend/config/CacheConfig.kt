c415efcd3e2fa3dd7a8c7b6d5ba85a079022b772
Remco Weekers <remco@weekers.dev>
2025-06-22 16:14:08 +0200

    feat: add caching

---
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/config/CacheConfig.kt
@@ -0,0 +1,26 @@
+package nl.orangeflamingo.voornameninliedjesbackend.config
+
+import com.github.benmanes.caffeine.cache.Caffeine
+import org.springframework.cache.CacheManager
+import org.springframework.cache.caffeine.CaffeineCacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.Duration
+
+@Configuration
+class CacheConfig {
+
+    @Bean
+    fun cacheManager(): CacheManager {
+        val caffeineCache = Caffeine.newBuilder()
+            .recordStats()
+            .maximumSize(5000)
+            .expireAfterWrite(Duration.ofDays(3))
+
+        return CaffeineCacheManager().apply {
+            isAllowNullValues = false
+            setCaffeine(caffeineCache)
+            cacheNames = listOf("songsByPrefix")
+        }
+    }
+}
