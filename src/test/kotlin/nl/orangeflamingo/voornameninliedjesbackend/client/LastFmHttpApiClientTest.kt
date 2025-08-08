2ac990e43c6af82c0029f0dbeeda0e82dbe057ac
Remco Weekers <remco@weekers.dev>
2025-08-08 11:05:12 +0200

    chore: exclude config files from test coverage

--- src/test/kotlin/nl/orangeflamingo/voornameninliedjesbackend/client/LastFmHttpApiClientTest.kt
+++ src/test/kotlin/nl/orangeflamingo/voornameninliedjesbackend/client/LastFmHttpApiClientTest.kt
@@ -1,109 +1,89 @@
 package nl.orangeflamingo.voornameninliedjesbackend.client
 
-import io.mockk.every
-import io.mockk.mockk
-import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmAlbumDto
-import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmArtistDto
 import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmError
-import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmResponseDto
-import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmTagDto
-import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmTopTagsDto
 import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmTrack
-import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmTrackDto
-import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmWikiDto
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.assertj.core.api.Assertions.fail
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.web.reactive.function.client.WebClient
-import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
 
 class LastFmHttpApiClientTest {
 
-    private val mockWebClient = mockk<WebClient>()
-    private val mockRequestHeadersUriSpec = mockk<WebClient.RequestHeadersUriSpec<*>>()
-    private val mockRequestHeadersSpec = mockk<WebClient.RequestHeadersSpec<*>>()
-    private val mockResponseSpec = mockk<WebClient.ResponseSpec>()
-
-    private val mockErrorRequestHeadersSpec = mockk<WebClient.RequestHeadersSpec<*>>()
-    private val mockErrorResponseSpec = mockk<WebClient.ResponseSpec>()
-
-    private val lastFmResponse: Mono<LastFmResponseDto> = Mono.just(
-        LastFmResponseDto(
-            track = LastFmTrackDto(
-                name = "Roxanne",
-                url = "https://www.last.fm/music/The+Police/_/Roxanne",
-                mbid = "8c2ead25-9d14-437b-aad2-cbc88958bf76",
-                artist = LastFmArtistDto(
-                    name = "The Police",
-                    url = "https://www.last.fm/music/The+Police",
-                    mbid = "9e0e2b01-41db-4008-bd8b-988977d6019a",
-                ),
-                album = LastFmAlbumDto(
-                    artist = "The Police",
-                    title = "Outlandos D'Amour",
-                    mbid = "8be3709f-25f8-4314-a39a-6df38798c35b",
-                    url = "https://www.last.fm/music/The+Police/Outlandos+D%27Amour"
-                ),
-                toptags = LastFmTopTagsDto(
-                    tag = listOf(
-                        LastFmTagDto(
-                            name = "rock",
-                            url = "https://www.last.fm/tag/rock"
-                        ),
-                        LastFmTagDto(
-                            name = "classic rock",
-                            url = "https://www.last.fm/tag/classic+rock"
-                        ),
-                        LastFmTagDto(
-                            name = "80s",
-                            url = "https://www.last.fm/tag/80s"
-                        )
-                    )
-                ),
-                wiki = LastFmWikiDto(
-                    summary = "Summary...",
-                    content = "Content..."
-                )
-            )
-        )
-    )
-    private val lastFmErrorResponse: Mono<LastFmResponseDto> = Mono.just(
-        LastFmResponseDto(
-            error = "6",
-            message = "Track not found",
-            track = null
-        )
-    )
-
-    private val lastFmHttpApiClient = LastFmHttpApiClient(
-        mockWebClient, "some-api-key"
-    )
+    private val mockWebServer = MockWebServer()
+    private lateinit var client: LastFmHttpApiClient
 
     @BeforeEach
-    fun init() {
-        every { mockWebClient.get() } returns mockRequestHeadersUriSpec
-        every { mockRequestHeadersUriSpec.uri(any(), eq("The Police"), eq("Roxanne")) } returns mockRequestHeadersSpec
-        every { mockRequestHeadersUriSpec.uri(any(), eq("The Police"), eq("Not actually Roxanne")) } returns mockErrorRequestHeadersSpec
-        every { mockRequestHeadersSpec.retrieve() } returns mockResponseSpec
-        every { mockResponseSpec.bodyToMono(LastFmResponseDto::class.java) } returns lastFmResponse
-        every { mockErrorRequestHeadersSpec.retrieve() } returns mockErrorResponseSpec
-        every { mockErrorResponseSpec.bodyToMono(LastFmResponseDto::class.java) } returns lastFmErrorResponse
+    fun setUp() {
+        mockWebServer.start()
+        val webClient = WebClient.builder()
+            .baseUrl(mockWebServer.url("/").toString())
+            .build()
+        client = LastFmHttpApiClient(webClient, "key")
     }
 
     @Test
-    fun `get track info`() {
-        val apiResponse = lastFmHttpApiClient.getTrack("The Police", "Roxanne").block()
-        assertThat(apiResponse).isInstanceOf(LastFmTrack::class.java)
-        val apiResponseTrack = apiResponse as LastFmTrack
-        assertEquals("https://www.last.fm/music/The+Police/_/Roxanne", apiResponseTrack.url)
+    fun `should parse regular response correctly`() {
+        val json =
+            """{"track":{"name":"Roxanne","url":"https://www.last.fm/music/The+Police/_/Roxanne","duration":"89000","streamable":{"#text":"0","fulltrack":"0"},"listeners":"1669955","playcount":"11291064","artist":{"name":"The Police","mbid":"9e0e2b01-41db-4008-bd8b-988977d6019a","url":"https://www.last.fm/music/The+Police"},"album":{"artist":"Sting","title":"The Very Best of Sting and The Police","url":"https://www.last.fm/music/Sting/The+Very+Best+of+Sting+and+The+Police","image":[{"#text":"https://lastfm.freetls.fastly.net/i/u/34s/1f7bd956c159483fc8d96511c5fc2cfb.png","size":"small"},{"#text":"https://lastfm.freetls.fastly.net/i/u/64s/1f7bd956c159483fc8d96511c5fc2cfb.png","size":"medium"},{"#text":"https://lastfm.freetls.fastly.net/i/u/174s/1f7bd956c159483fc8d96511c5fc2cfb.png","size":"large"},{"#text":"https://lastfm.freetls.fastly.net/i/u/300x300/1f7bd956c159483fc8d96511c5fc2cfb.png","size":"extralarge"}]},"toptags":{"tag":[{"name":"rock","url":"https://www.last.fm/tag/rock"},{"name":"classic rock","url":"https://www.last.fm/tag/classic+rock"},{"name":"80s","url":"https://www.last.fm/tag/80s"},{"name":"new wave","url":"https://www.last.fm/tag/new+wave"},{"name":"The Police","url":"https://www.last.fm/tag/The+Police"}]},"wiki":{"published":"30 Nov 2008, 16:15","summary":"\"Roxanne\" is a hit song by the rock band The Police, first released in 1978 as a single and on their album Outlandos d'Amour. It was written about a prostitute in southern France.\n\nPolice lead singer Sting wrote the song, inspired by the prostitutes he saw near the band's seedy hotel while in Paris, France in October 1977 to perform at the Nashville Club. The title of the song comes from the name of the character in the play Cyrano de Bergerac, an old poster of which was hanging in the hotel foyer. <a href=\"http://www.last.fm/music/The+Police/_/Roxanne\">Read more on Last.fm</a>.","content":"\"Roxanne\" is a hit song by the rock band The Police, first released in 1978 as a single and on their album Outlandos d'Amour. It was written about a prostitute in southern France.\n\nPolice lead singer Sting wrote the song, inspired by the prostitutes he saw near the band's seedy hotel while in Paris, France in October 1977 to perform at the Nashville Club. The title of the song comes from the name of the character in the play Cyrano de Bergerac, an old poster of which was hanging in the hotel foyer.\n\nSting had originally conceived the song as a bossa nova, although he credits Police drummer Stewart Copeland for suggesting its final rhythmic form as a tango. During recording, Sting accidentally sat down on a piano keyboard in the studio, resulting in the atonal piano chord and laughter preserved at the beginning of the track. The Police were initially diffident about the song, but Miles Copeland III was immediately enthusiastic after hearing it, becoming their manager and getting them their first record deal with A&M Records. The single did not chart at first, but it was re-released in April 1979 and reached #12 in the UK and #32 in the U.S., and went on to become one of the classic Police songs as well as a staple of Sting's performances during his solo career. \"Roxanne\" has appeared on every single one of The Police's Greatest hits albums.\n\nRolling Stone ranked it #388 on their list of the 500 Greatest Songs of All Time.\n\nThis was also the appropriately first song the band performed live (at the 2007 Grammy Awards) to kick off their 30th Anniversary Reunion Tour. <a href=\"http://www.last.fm/music/The+Police/_/Roxanne\">Read more on Last.fm</a>. User-contributed text is available under the Creative Commons By-SA License; additional terms may apply."}}}"""
+        mockWebServer.enqueue(MockResponse()
+            .setBody(json)
+            .addHeader("Content-Type", "application/json")
+            .setResponseCode(200)
+        )
+
+        StepVerifier.create(client.getTrack("The Police", "Roxanne"))
+            .assertNext { result ->
+                when (result) {
+                    is LastFmTrack -> {
+                        assertThat(result.url)
+                            .isEqualTo("https://www.last.fm/music/The+Police/_/Roxanne")
+                        assertThat(result.name)
+                            .isEqualTo("Roxanne")
+                    }
+                    is LastFmError -> fail("Expected LastFmTrack but got LastFmError: ${result.message}")
+                    else -> fail("Unexpected type: ${result::class.simpleName}")
+                }
+            }
+            .verifyComplete()
     }
 
     @Test
-    fun `track info not found`() {
-        val apiResponse = lastFmHttpApiClient.getTrack("The Police", "Not actually Roxanne").block()
-        assertThat(apiResponse).isInstanceOf(LastFmError::class.java)
-        val apiResponseTrack = apiResponse as LastFmError
-        assertEquals("6", apiResponseTrack.code)
+    fun `should parse error response correctly`() {
+        val json = """{
+            "error": 6,
+            "message": "Track not found",
+            "links": []
+        }"""
+
+        mockWebServer.enqueue(MockResponse()
+            .setBody(json)
+            .addHeader("Content-Type", "application/json")
+            .setResponseCode(200)
+        )
+
+        StepVerifier.create(client.getTrack("The Police", "Roxanne"))
+            .assertNext { result ->
+                when (result) {
+                    is LastFmError -> {
+                        assertThat(result.code)
+                            .isEqualTo("6")
+                        assertThat(result.message)
+                            .isEqualTo("Track not found")
+                    }
+                    is LastFmTrack -> fail("Expected LastFmError but got LastFmTrack: ${result.artist} - ${result.name}")
+                    else -> fail("Unexpected type: ${result::class.simpleName}")
+                }
+            }
+            .verifyComplete()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        mockWebServer.shutdown()
     }
 }
