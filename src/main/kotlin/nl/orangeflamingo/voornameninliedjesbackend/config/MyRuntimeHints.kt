0155aa6ecfd6eee5ae0a09d9dbb5bb2167b164ab
Remco Weekers <remco@weekers.dev>
2026-08-15 12:22:10 +0200

    fix(native): register runtime hints for Last.fm DTOs

--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/config/MyRuntimeHints.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/config/MyRuntimeHints.kt
@@ -1,5 +1,12 @@
 package nl.orangeflamingo.voornameninliedjesbackend.config
 
+import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmAlbumDto
+import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmArtistDto
+import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmResponseDto
+import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmTagDto
+import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmTopTagsDto
+import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmTrackDto
+import nl.orangeflamingo.voornameninliedjesbackend.domain.LastFmWikiDto
 import nl.orangeflamingo.voornameninliedjesbackend.dto.ImageDimensionsDto
 import nl.orangeflamingo.voornameninliedjesbackend.dto.ImageHashDto
 import org.springframework.aot.hint.RuntimeHints
@@ -11,5 +18,12 @@ class MyRuntimeHints: RuntimeHintsRegistrar {
         // Register serialization
         hints.reflection().registerType(TypeReference.of(ImageDimensionsDto::class.java))
         hints.reflection().registerType(TypeReference.of(ImageHashDto::class.java))
+        hints.reflection().registerType(TypeReference.of(LastFmResponseDto::class.java))
+        hints.reflection().registerType(TypeReference.of(LastFmTrackDto::class.java))
+        hints.reflection().registerType(TypeReference.of(LastFmArtistDto::class.java))
+        hints.reflection().registerType(TypeReference.of(LastFmAlbumDto::class.java))
+        hints.reflection().registerType(TypeReference.of(LastFmTopTagsDto::class.java))
+        hints.reflection().registerType(TypeReference.of(LastFmTagDto::class.java))
+        hints.reflection().registerType(TypeReference.of(LastFmWikiDto::class.java))
     }
 }
