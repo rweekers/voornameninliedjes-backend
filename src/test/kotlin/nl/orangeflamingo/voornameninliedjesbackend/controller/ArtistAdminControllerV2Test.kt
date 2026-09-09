c5fb8f121af17b85d8527d657bcbe5e2b6d95be5
Remco Weekers <remco@weekers.dev>
2026-09-09 19:11:15 +0200

    tmp

--- src/test/kotlin/nl/orangeflamingo/voornameninliedjesbackend/controller/ArtistAdminControllerV2Test.kt
+++ src/test/kotlin/nl/orangeflamingo/voornameninliedjesbackend/controller/ArtistAdminControllerV2Test.kt
@@ -1,30 +1,38 @@
 package nl.orangeflamingo.voornameninliedjesbackend.controller
 
+import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import nl.orangeflamingo.voornameninliedjesbackend.command.CreateArtistCommand
+import nl.orangeflamingo.voornameninliedjesbackend.config.CorsConfig
 import nl.orangeflamingo.voornameninliedjesbackend.domain.Artist
 import nl.orangeflamingo.voornameninliedjesbackend.service.ArtistService
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest
 import org.springframework.cache.CacheManager
 import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
-import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(ArtistAdminControllerV2::class)
-@Disabled
+@EnableConfigurationProperties(CorsConfig::class)
+@TestPropertySource(
+    properties = [
+        "voornameninliedjes.cors.domains.allowed.api[0]=http://localhost",
+        "voornameninliedjes.cors.domains.allowed.admin[0]=http://localhost"
+    ]
+)
 class ArtistAdminControllerV2Test {
 
     @Autowired private lateinit var mockMvc: MockMvc
-    @MockitoBean private lateinit var artistService: ArtistService
-    @MockitoBean private lateinit var cacheManager: CacheManager
+    @MockkBean private lateinit var artistService: ArtistService
+    @MockkBean private lateinit var cacheManager: CacheManager
 
     @Test
     @WithMockUser(roles = ["ADMIN"])
@@ -35,6 +43,14 @@ class ArtistAdminControllerV2Test {
         )
 
         every { artistService.create(any(CreateArtistCommand::class)) } returns artist
+
+        val result = mockMvc.perform(
+            post("/admin/artists")
+            // ...
+        ).andReturn()
+
+        println(result.resolvedException)
+        result.resolvedException?.printStackTrace()
 
         mockMvc.perform(
             post("/admin/artists")
