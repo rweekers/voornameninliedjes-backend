f1dc970cf47b6432090d8226d29bfe4660e47caf
Remco Weekers <remco@weekers.dev>
2026-09-17 22:44:26 +0200

    tmp

--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/config/SecurityConfig.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackend/config/SecurityConfig.kt
@@ -25,44 +25,72 @@ class SecurityConfig(
     private val userRepository: UserRepository,
     private val authenticationEntryPoint: MyBasicAuthPoint
 ) {
-    @Bean
-    fun filterChain(
-        http: HttpSecurity
-    ): SecurityFilterChain {
-        http
-            .cors(withDefaults())
-            .csrf { it.disable() }
-            .authorizeHttpRequests { requests ->
-                requests
-                    .requestMatchers("/admin/authenticate").permitAll()
-                    .requestMatchers(HttpMethod.POST).authenticated()
-                    .requestMatchers(HttpMethod.PUT).authenticated()
-                    .requestMatchers(HttpMethod.PATCH).authenticated()
-                    .requestMatchers(HttpMethod.DELETE).authenticated()
-                    .requestMatchers("/admin/**").authenticated()
-                    .anyRequest().permitAll()
-            }
-            .httpBasic(withDefaults())
-        return http.build()
-    }
 
-    @Bean
-    fun httpBasicConfigurer(): HttpBasicConfigurer<out HttpSecurityBuilder<*>> {
-        return HttpBasicConfigurer()
-            .authenticationEntryPoint(authenticationEntryPoint)
-    }
+    @Configuration
+    @EnableWebSecurity
+    @EnableMethodSecurity
+    class SecurityConfig {
 
-    @Bean
-    fun userDetailsService(): UserDetailsService {
-        return UserDetailsService { username ->
-            val user = userRepository.findByUsername(username) ?: throw UsernameNotFoundException(username)
-            MyUserPrincipal(user)
+        @Bean
+        fun filterChain(http: HttpSecurity): SecurityFilterChain {
+            http
+                .cors(withDefaults())
+                .csrf { it.disable() }
+                .authorizeHttpRequests { requests ->
+                    requests
+                        .requestMatchers("/admin/authenticate").permitAll()
+                        .requestMatchers(HttpMethod.POST).authenticated()
+                        .requestMatchers(HttpMethod.PUT).authenticated()
+                        .requestMatchers(HttpMethod.PATCH).authenticated()
+                        .requestMatchers(HttpMethod.DELETE).authenticated()
+                        .requestMatchers("/admin/**").authenticated()
+                        .anyRequest().permitAll()
+                }
+                .oauth2ResourceServer { oauth2 ->
+                    oauth2.jwt(withDefaults())
+                }
+
+            return http.build()
         }
     }
 
-    @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
-    }
-
+//    @Bean
+//    fun filterChain(
+//        http: HttpSecurity
+//    ): SecurityFilterChain {
+//        http
+//            .cors(withDefaults())
+//            .csrf { it.disable() }
+//            .authorizeHttpRequests { requests ->
+//                requests
+//                    .requestMatchers("/admin/authenticate").permitAll()
+//                    .requestMatchers(HttpMethod.POST).authenticated()
+//                    .requestMatchers(HttpMethod.PUT).authenticated()
+//                    .requestMatchers(HttpMethod.PATCH).authenticated()
+//                    .requestMatchers(HttpMethod.DELETE).authenticated()
+//                    .requestMatchers("/admin/**").authenticated()
+//                    .anyRequest().permitAll()
+//            }
+//            .httpBasic(withDefaults())
+//        return http.build()
+//    }
+//
+//    @Bean
+//    fun httpBasicConfigurer(): HttpBasicConfigurer<out HttpSecurityBuilder<*>> {
+//        return HttpBasicConfigurer()
+//            .authenticationEntryPoint(authenticationEntryPoint)
+//    }
+//
+//    @Bean
+//    fun userDetailsService(): UserDetailsService {
+//        return UserDetailsService { username ->
+//            val user = userRepository.findByUsername(username) ?: throw UsernameNotFoundException(username)
+//            MyUserPrincipal(user)
+//        }
+//    }
+//
+//    @Bean
+//    fun passwordEncoder(): PasswordEncoder {
+//        return BCryptPasswordEncoder()
+//    }
 }
